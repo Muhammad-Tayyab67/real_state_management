a6c773154971ecf28b9b95a0387ba905a8681f72
Muhammad Tayyab <mt6780175@gmail.com>
2023-09-03 22:11:12 +0500

    AuthApis Created

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Sanctum\PersonalAccessToken;
 
 class AuthController extends Controller
 {
@@ -45,6 +47,73 @@ class AuthController extends Controller
             'user' => $user,
             'access_token' => $token,
             'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        // Validating request data
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        // returning response on validation fail
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors()->first(),
+            ]);
+        }
+
+        // finding user by email
+        $user = User::where('email', $request->email)->first();
+
+        // returning response if user not found
+        if (!$user) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid Email or Password',
+            ]);
+        }
+
+        // checking password
+        if (!Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Invalid Email or Password',
+            ]);
+        }
+
+        // generating token
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        //logging user
+        Auth::guard()->login($user);
+
+        // returning response
+        return response()->json([
+            'success' => true,
+            'user' => $user,
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+
+    }
+
+    // logout function
+    public function logout(Request $request)
+    {
+        $user = $request->user();
+
+        if ($token = $request->bearerToken()) {
+            $authToken = PersonalAccessToken::findToken($token);
+            $authToken->delete();
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'user has been logged out!',
         ]);
     }
 }
