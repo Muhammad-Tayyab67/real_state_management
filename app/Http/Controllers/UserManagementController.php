577a76af500bb1088530ac153569f7029eaada73
Muhammad Tayyab <mt6780175@gmail.com>
2023-10-04 23:38:17 +0500

    User ManageMent init

---
+++ app/Http/Controllers/UserManagementController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Support\Facades\{Validator, Storage};
+
+use Illuminate\Http\Request;
+
+class UserManagementController extends Controller
+{
+    //get all users
+    public function list()
+    {
+        $users = User::all();
+        return response()->json([
+            'success' => true,
+            'users' => $users,
+        ]);
+    }
+
+    //get single user
+    public function show($id)
+    {
+        $user = User::find($id);
+        if (!$user) {
+            return response()->json([
+                'success' => false,
+                'message' => 'User not found',
+            ]);
+        }
+        return response()->json([
+            'success' => true,
+            'user' => $user,
+        ]);
+    }
+
+    //update user
+    public function update(Request $request, $id)
+    {
+        // Validating request data
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'userType' => 'required|string|max:255',
+        ]);
+
+        // returning error message on validation fail
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors()->first(),
+            ]);
+        }
+
+        // finding user
+        $user = User::find($id);
+        if (!$user) {
+            return response()->json([
+                'success' => false,
+                'message' => 'User not found',
+            ]);
+        }
+
+        //profile picture
+        if ($request->hasFile('profilePicture')) {
+
+            if ($user->profilePicture) {
+                //Deleting existing image
+                $existingImagePath = basename($user->profilePicture);
+
+                Storage::delete('public/profilePictures/'. $user->id. '/' . $existingImagePath);
+            }
+            //Storing image and returning its url to diplays in frontend react app
+            $path = $request->file('profilePicture')->store('public/profilePictures/'.$user->id.'/');
+
+            //get img src url
+            $url = asset('storage/profilePictures/' . basename($path));
+
+            //updating user
+            $user->update([
+                'profilePicture' => $url,
+            ]);
+        }
+
+        // updating user
+        $user->update([
+            'name' => $request->name,
+            'userType' => $request->userType,
+        ]);
+
+        // returning response
+        return response()->json([
+            'success' => true,
+            'user' => $user,
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\UserManagementController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,7 +20,22 @@ Route::post('/register', [AuthController::Class, 'register']);
 Route::post('/login', [AuthController::Class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/users', [UserManagementController::class, 'list']);
+    Route::get('/user/show/{id}', [UserManagementController::class, 'show']);
+    Route::post('/user/update/{id}', [UserManagementController::class, 'update']);
+});
+
+//Public API to display images
+Route::get('/images/{filename}', function ($filename) {
+    $path = storage_path('app/public/profilePictures' . $filename);
+    if (!File::exists($path)) {
+        abort(402);
+    }
+    $file = File::get($path);
+    $type = File::mimeType($path);
+    $response = Response::make($file, 200);
+    $response->header('Content-Type', $type);
+    return $response;
 });
 
